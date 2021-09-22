b65e1bc8d89b2dd062228bf4c58252a5fe1dd2de
PavelNevolin <91225347+PavelNevolin@users.noreply.github.com>
2021-09-23 00:39:54 +0300

    Add files via upload

---
+++ main4.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style>
+		body{
+			margin:0;
+			background-size: 100%;
+		}
+		a {
+			text-decoration: none;
+		}
+
+		li{
+			list-style-type: none;
+			display: inline;
+		}
+		
+		
+
+	.logo{
+		width: 370px ;
+		font-size: 20px;
+		margin-right: 17%;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+	.menu{
+		width:150px;
+		margin-right: 10px;
+		margin-top:20px;
+		font-size: 20px;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+
+	.menu1{
+		width:100px;
+		font-size: 15px;
+		font-family: Helvetica;
+		margin:-2px;
+		display: inline-block;
+		white-space: nowrap;
+		padding:1.5% 16.57% 1.5% 30px;
+		background-color:#08b2d6;
+	} 
+
+	.menu3{
+		width:100px;
+		font-size: 15px;
+		font-family: Helvetica;
+		margin:-2px;
+		display: inline-block;
+		white-space: nowrap;
+		padding:1.5% 16.57% 1.5% 30px;
+		background-color:#38c2e6;
+	} 
+
+	.menu12{
+		margin-top: 11.5%;
+		margin-left:10%;
+		margin-right:10%;
+	} 
+
+	.name1{
+		align-content: middle;
+		font-size: 55px;
+		font-family: Helvetica;
+		margin-left:30%;
+		color: #000;
+		line-height: 2;
+
+	} 
+</style>
+<body>
+	<div class="name1">
+		<b>
+			ОБ УЧЕРЕЖДЕНИИ
+		</b>
+	</div>
+
+</body>
