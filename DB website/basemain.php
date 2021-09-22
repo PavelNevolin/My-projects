5f1b18e8081985bd932d06b3ffe70f7d06a05bac
PavelNevolin <91225347+PavelNevolin@users.noreply.github.com>
2021-09-23 01:14:51 +0300

    Add files via upload

---
+++ DB website/basemain.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style>
+		body{
+			margin:0;
+		}
+		a {
+			text-decoration: none;
+			}
+			li{
+				list-style-type: none;
+				display: inline;
+			}
+
+	.logo{
+		width: 370px ;
+		font-size: 20px;
+		margin-right: 17%;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+	.menu{
+		width:150px;
+		margin-right: 10px;
+		margin-top:20px;
+		font-size: 20px;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+	</style>
+</head>
+<iframe src="main2.php"  width="100%"  scrolling="no"  style="border-width: 0px; height: 8%; " ></iframe>
+<iframe src="main3.php" align="middle" width="100%"  height="700px" scrolling="no"  style="border-width: 0px; margin-bottom:15px;">
+</iframe>
+</br>
+<iframe src="main4.php" align="middle" width="100%"  height="1000px" scrolling="no"  style="border-width: 0px;">
+</iframe>
+ <!-- <div style =" position: realtive;">
+<iframe src="menu.html" height="1080px"  scrolling="no" style="width:16%; border-width: 0px; float: left; box-sizing: border-box; display: inline-block;"></iframe>
+<iframe name = "right" src="iPhone.html" height="1080px" style="width: 84%;border-width: 0px;  box-sizing: border-box; display: inline-block; position: absolute; background-color:#5F95D0;  background-repeat: no-repeat"  scrolling="no"></iframe> */
+</div>
+<iframe  src="bottom.html"  width="100%" style="border-width: 0px; border-color: black;"  scrolling="no"></iframe> -->
+
+</div>
+</font>
+</body>
+
+
+</html>
---
+++ DB website/main3.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<style>
+		body{
+			margin:0;
+			background-size: 100%;
+		}
+		a {
+			text-decoration: none;
+		}
+
+		li{
+			list-style-type: none;
+			display: inline;
+		}
+		div:hover{
+			background-color:#083b47;
+		}
+		
+
+	.logo{
+		width: 370px ;
+		font-size: 20px;
+		margin-right: 17%;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+	.menu{
+		width:150px;
+		margin-right: 10px;
+		margin-top:20px;
+		font-size: 20px;
+		font-family: Helvetica;
+		border: 3px solid #08b2d6;
+		padding:10px;
+		color: #08b2d6;
+	} 
+
+	.menu1{
+		width:100px;
+		font-size: 15px;
+		font-family: Helvetica;
+		margin:-2px;
+		display: inline-block;
+		white-space: nowrap;
+		padding:1.5% 16.45% 1.5% 30px;
+		background-color:#08b2d6;
+	} 
+
+	.menu3{
+		width:100px;
+		font-size: 15px;
+		font-family: Helvetica;
+		margin:-2px;
+		display: inline-block;
+		white-space: nowrap;
+		padding:1.5% 16.56% 1.5% 30px;
+		background-color:#38c2e6;
+	} 
+
+	.menu12{
+		margin-top: 11.5%;
+		margin-left:10%;
+		margin-right:10%;
+	} 
+
+	.name1{
+		align-content: middle;
+		font-size: 55px;
+		font-family: Helvetica;
+		margin:0% 5% 0% 60%;
+		color: #fff;
+		line-height: 2;
+
+	} 
+
+	</style>
+</head>
+<body width="1920px" background="фон1.jpg">
+	<div class="name1" align="right" style="background-color:rgba(0,0,0,0);">
+		"МЫ</br>СТОИМ НА СТРАЖЕ</br> ВАШЕГО ЗДОРОВЬЯ"
+	</div>
+
+<div class="menu12">
+	<a style="color: #fff;" href="/">
+		<div class="menu1" >
+			<img width="60px" height="55px" src="вирус.png">
+			<p>Тесты на COVID-19</p>
+		</div>
+	</a>
+
+	<a style="color: #fff;" href="/">
+		<div class="menu3" >
+			<img width="60px" height="55px" src="крест.png">
+			<p>Центр здоровья</p>
+		</div>
+	</a>
+
+	<a style="color: #fff;" href="/">
+		<div class="menu1">
+			<img width="60px" height="55px" src="таблетки.png">
+			<p>Аптечные пункты</p>
+		</div>
+	</a>
+
+	<a style="color: #fff;" href="/registration1.php">
+		<div class="menu3">
+			<img width="60px" height="55px" src="блокнот.png">
+			<p>Записаться на приём</p>
+		</div>
+	</a>
+</div>
+
+</body>
